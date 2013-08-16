71168174ed9827fa026614568c12c8c49dcea652
Mathieu Levesque-Lavallee <mathieu.levesque.lavallee@gmail.com>
2013-08-16 17:15:36 -0400

    Try to call dot from sinatra

--- app/graphviz_server.rb
+++ app/graphviz_server.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 
 class GraphvizServer < Sinatra::Base
+  #use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  #  [username, password] == ['admin', 'admin']
+  #end
+
   get '/' do
-    'Hello world!'
+    output = ''
+
+    IO.popen "dot -Tsvg", 'w+' do |f|
+      f.puts 'digraph G {}'
+      output = f.gets
+      puts output
+    end
+
+    [200, {"Content-Type" => "text/plain"}, [output]]
   end
 end
